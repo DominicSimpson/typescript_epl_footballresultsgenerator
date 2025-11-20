7535ac325f68fec33879d96e1666959dd791120e
db_simpson@yahoo.co.uk <db_simpson@yahoo.co.uk>
2025-11-20 10:01:37 +0000

    Encoding raw data

--- build/index.js
+++ build/index.js
@@ -1,2 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+// encoding the raw data so that it can be processed - returns as one large string
+const matches = fs_1.default.readFileSync('football.csv', {
+    encoding: 'utf-8'
+});
+console.log(matches);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 import fs from 'fs';
+
+// encoding the raw data so that it can be processed - returns as one large string
+const matches = fs.readFileSync('football.csv', {
+    encoding: 'utf-8'
+})
+    .split('\n')
+    .map(
+        (row: string): string[] => {
+        return row.split(',');  
+    }
+); // created array of arrays
+
+console.log(matches);
+
