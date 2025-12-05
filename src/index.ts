55cb95a0c84252560631c916fcb81d7896c30727
db_simpson@yahoo.co.uk <db_simpson@yahoo.co.uk>
2025-12-05 11:51:18 +0000

    Described a row with tuple

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from './MatchResult';
 
+
 const reader = new CsvFileReader('football.csv');
 reader.read();
-
-console.log(reader.data);
-
+ 
 let manUnitedWins = 0;
 
 
